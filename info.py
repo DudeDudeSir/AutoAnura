efb614e7ec7664e0a4673d979ca39dd233666824
RAHUL <88625008+DudeDudeSir@users.noreply.github.com>
2023-01-21 11:00:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -82,11 +82,6 @@ if SELF_DELETE == "True":
     SELF_DELETE = True
 
     # Download Tutorial Button #
-DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
-DOWNLOAD_TEXT_URL = "https://t.me/sources_cods/55"
-
-   # Custom Caption Under Button #
-CAPTION_BUTTON = "Suscribe"
-CAPTION_BUTTON_URL = "https://youtube.com/channel/UCqts9WhhlioK3RB9XQQzoAg"
+###########DUDE##########
 
    # Auto Delete For Bot Sending Files #
